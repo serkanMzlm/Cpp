f689dcac083a88362bf7485db9b443bbac1b5b8a
serkanMzlm <74418302+serkanMzlm@users.noreply.github.com>
2021-01-12 20:39:02 +0300

    Update class_friend.cpp

--- OOP/class_friend.cpp
+++ OOP/class_friend.cpp
@@ -6,13 +6,13 @@ class Tarih {
 		string  ay;
 		int gun;
 	public : 
-		Tarih(int , string ,int); //constructor tanýmladýk 
-		friend class  Yaz; // arkadaþ sýnýfý 
+		Tarih(int , string ,int); //constructor tanimladik 
+		friend class  Yaz; // arkadas sinif 
 	
 };
 
 Tarih::Tarih(int yil , string ay , int  gun ){
-	this -> yil = yil; // ayný adlar kullanýldýðý içiin 'this pointer' yararlandýk 
+	this -> yil = yil; // ayni adlar kullanildiğı için 'this pointer' yararlandik 
 	this -> ay = ay;
 	this -> gun = gun;
 }
@@ -30,7 +30,7 @@ cout << yaz.gun << "/" << yaz.ay << "/" << yaz.yil << endl;
 main(){
 
 	Tarih tarih(2020,"Aralik",23);
-	Yaz::ekrana_yaz(tarih); // static  bir fonksiyon oldugu için '::' eriþebiliriz
+	Yaz::ekrana_yaz(tarih); // static  bir fonksiyon oldugu için '::' eribebiliriz
 
 	return 0;
 }
